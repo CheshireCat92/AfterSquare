50500033f9659d5d38d93a856d835e24291756d5
Cheshire <crazycheshirecat92@gmail.com>
2015-08-07 02:15:46 +0300

    - Added location Manager class - added helper category (callback for selectors)

---
+++ AfterSquare/AfterSquare/LocationManager.h
@@ -0,0 +1,13 @@
+//
+//  LocationManager.h
+//  AfterSquare
+//
+//  Created by Cheshire on 06.08.15.
+//  Copyright (c) 2015 Cheshire. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocationManager : NSObject
+
+@end
---
+++ AfterSquare/AfterSquare/LocationManager.m
@@ -0,0 +1,13 @@
+//
+//  LocationManager.m
+//  AfterSquare
+//
+//  Created by Cheshire on 06.08.15.
+//  Copyright (c) 2015 Cheshire. All rights reserved.
+//
+
+#import "LocationManager.h"
+
+@implementation LocationManager
+
+@end
